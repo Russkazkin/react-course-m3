965366f89de6423c32b21d87877438634a58b3ce
Russkazkin <ruslan@skazkin.su>
2020-11-24 18:16:25 +0300

    Add Person component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Person from "./Person/Person";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World! I'm React App</h1>
+        <h1>Hello World! I'm React App</h1>
+        <p>This is really working!</p>
+        <Person />
     </div>
   );
 }
