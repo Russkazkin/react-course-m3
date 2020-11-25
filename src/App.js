ff0baa3aea7e3d96dad87d6ac218be47d50bc55c
Russkazkin <ruslan@skazkin.su>
2020-11-25 19:42:16 +0300

    Add props to the Person component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
     <div className="App">
         <h1>Hello World! I'm React App</h1>
         <p>This is really working!</p>
-        <Person />
+        <Person name="Ruslan " age="45" />
+        <Person name="Julia" age="46" />
+        <Person name="Alex" age="19" />
+        <Person name="Masha" age="9">I like cats!</Person>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,5 @@
-const person = () => {
-    return <p>I'm a person and I'm years {Math.floor(Math.random() * 30)} old!</p>;
+const person = ({name, age, children}) => {
+    return <p>I'm {name} and I'm years {age} years old!</p>;
 }
 
 export default person;
