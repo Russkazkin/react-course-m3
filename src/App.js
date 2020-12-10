4a24b0239cd962ed9a558495c960a9996c72ade9
Russkazkin <ruslan@skazkin.su>
2020-12-10 17:29:58 +0300

    Add new nameChangedHandler

--- src/App.js
+++ src/App.js
@@ -27,6 +27,22 @@ class App extends Component {
         });
     }
 
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex(item => {
+            return item.id === id;
+        });
+
+        const person = {...this.state.persons[personIndex]};
+
+        person.name = event.target.value;
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState({
+            persons: persons,
+        })
+    }
+
 
     render() {
         const style = {
@@ -39,7 +55,10 @@ class App extends Component {
             persons = (
                 <div>
                     {this.state.persons.map((person, index) => {
-                        return <Person {...person} click={() => this.deletePersonHandler(index)} key={person.id}/>
+                        return <Person {...person}
+                                       click={() => this.deletePersonHandler(index)}
+                                       changed={(event) => this.nameChangedHandler(event, person.id)}
+                                       key={person.id}/>
                     })}
                 </div>
             );
