fd95aad5f90d39a0fc06384af60573a55864c873
Russkazkin <ruslan@skazkin.su>
2020-12-16 17:28:55 +0300

    Style with styled-components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,17 +1,21 @@
 import './Person.sass';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+            @media (max-width: 767px) {
+                width: 90%;
+            }
+        `;
 
 const Person = ({name, age, click, changed, children}) => {
-    const style = {
-
-    };
     return (
-        <div className="Person card" style={style}>
+       <StyledDiv className="Person">
             <div className="card-body">
                 <p onClick={click}>I'm {name} and I'm years {age} years old!</p>
                 <p>{children}</p>
                 <input className="form-control" onChange={changed} value={name}/>
             </div>
-        </div>
+        </StyledDiv>
     );
 }
 
