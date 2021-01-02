f875421de80b516707b2f6c04ccda73ec42daf66
Russkazkin <ruslan@skazkin.su>
2021-01-02 06:23:12 +0300

    Make class based components from Person and Persons

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
 import styles from './Person.module.sass';
 import styled from 'styled-components';
 
 const StyledDiv = styled.div`
-            @media (max-width: 767px) {
-                width: 90%;
-            }
-        `;
+  @media (max-width: 767px) {
+    width: 90%;
+  }
+`;
 
-const Person = ({name, age, click, changed, children}) => {
-    return (
-       <StyledDiv className={'card ' + styles.Person}>
-            <div className="card-body">
-                <p onClick={click}>I'm {name} and I'm years {age} years old!</p>
-                <p>{children}</p>
-                <input className="form-control" onChange={changed} value={name}/>
-            </div>
-        </StyledDiv>
-    );
+class Person extends Component {
+    render() {
+        const {name, age, click, changed, children} = this.props;
+        return (
+            <StyledDiv className={'card ' + styles.Person}>
+                <div className="card-body">
+                    <p onClick={click}>I'm {name} and I'm years {age} years old!</p>
+                    <p>{children}</p>
+                    <input className="form-control" onChange={changed} value={name}/>
+                </div>
+            </StyledDiv>
+        );
+    }
 }
 
 export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 import Person from "./Person/Person";
 
-const Persons = ({persons, deletePersonHandler, nameChangedHandler}) => persons.map((person, index) => {
-        return <ErrorBoundary key={person.id}>
-            <Person {...person}
-                    click={() => deletePersonHandler(index)}
-                    changed={(event) => nameChangedHandler(event, person.id)}/>
-        </ErrorBoundary>
+class Persons extends Component {
+    render() {
+        const {persons, deletePersonHandler, nameChangedHandler} = this.props;
+        return persons.map((person, index) => (
+            <ErrorBoundary key={person.id}>
+                <Person {...person}
+                        click={() => deletePersonHandler(index)}
+                        changed={(event) => nameChangedHandler(event, person.id)}/>
+            </ErrorBoundary>
+        ));
     }
-);
+}
 
 export default Persons;
