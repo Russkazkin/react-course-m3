7567f47307c283d0554adfe56a88fef160f496f2
Russkazkin <ruslan@skazkin.su>
2021-01-07 14:10:16 +0300

    Add ref for toggle button in the Cockpit

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 import styled from "styled-components";
 
 const style = {
@@ -7,20 +7,30 @@ const style = {
 
 const StyledDiv = styled.div`
   color: #777;
+
   h1 {
     color: ${props => props.alt ? 'red' : '#000'};
   }
 `;
 
-const Cockpit = ({showPersons, togglePersonsHandler, title}) => (
-    <StyledDiv>
-        <h1>Hello World! I'm {title}</h1>
-        <p>This is really working!</p>
-        <button className={"btn " + (showPersons ? 'btn-danger' : 'btn-success')}
-                style={style}
-                onClick={togglePersonsHandler}>Toggle Persons
-        </button>
-    </StyledDiv>
-);
+const Cockpit = ({showPersons, togglePersonsHandler, title}) => {
+    const toggleButton = useRef(null);
+
+    useEffect(() => {
+        toggleButton.current.click();
+    }, []);
+
+    return (
+        <StyledDiv>
+            <h1>Hello World! I'm {title}</h1>
+            <p>This is really working!</p>
+            <button ref={toggleButton}
+                    className={"btn " + (showPersons ? 'btn-danger' : 'btn-success')}
+                    style={style}
+                    onClick={togglePersonsHandler}>Toggle Persons
+            </button>
+        </StyledDiv>
+    );
+}
 
 export default Cockpit;
